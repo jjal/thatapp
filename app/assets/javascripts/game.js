47f318c2763e8ade37e9b99558a78f1c8bde1eff
futuresandwich <lorenzon@gmail.com>
2012-12-21 18:45:07 +0700

    separatinng game js

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,132 +1,135 @@
 var currentWindow;
 var currentQuestionId = 0;
+function Game(gameId, userId) {
 
-function loop()
-{
-  $.getJSON( "/games/"+getGameId()+".json", function(data) {
-    update(data);
-  });
-  setTimeout(loop, 2000);
-}
-
-function update(game)
-{
-  showCurrentQuestion(game.current_question_id);
-  switch(game.state)
-  {
-    case 0: //open
-      // i'm the questionner and it's open so show the questions
-      if(game.current_user_id == getCurrentUserId())
-      {
-        showQuestions();
-      } else {
-        showWaitForQuestionMessage();
-      }
-    break;
-    case 1:
-      // i'm the questionner and it's closed so show the wait for answer message
-      if(game.current_user_id == getCurrentUserId())
-      {
-        showWaitForAnswerMessage();
-      } else {
-        showAnswers();
-      }
-    break;
-    case 3:
-      showSuccess();
-      showNextButton(game.current_user_id);
-    break;
-    case 4:
-      showFail(game.answer_id);
-      showNextButton(game.current_user_id);
-    break;
-    default:
-    break;
-  }
-}
-
-function getGameId()
-{
-  return <%= @game.id %>
-}
-
-function getCurrentUserId()
-{
-  return <%= current_user.id %>
-}
-
-function showNextButton(current_user)
-{
-  if(current_user == getCurrentUserId())
-    $("#next_question").show();
-  else
-    $("#next_question").hide();
-}
-
-function showCurrentQuestion(id)
-{
-
-  if(!id)
-    return;
-  if(id != currentQuestionId)
-    $.get("/questions/"+id, function(html) {
-      $("#question_space").html(html);
-    });
-  currentQuestionId = id;
-}
-
-function showQuestions()
-{
-  $("#next_question").hide();
-  if(currentWindow == "questions")
-    return;
+  this.id = gameId;
+  this.currentUserId = userId;
   
-  $.get("/questions?game_id="+getGameId(), function (html) {
-    $("#action_space").html(html);
-  });
-  $("#status_space").html("Choose a question!");
-  currentWindow = "questions";
-}
+  this.tick = function()
+  {
+    $.getJSON( "/games/"+this.getGameId()+".json", function(data) {
+      game.update(data);
+    });
+  }
+  
+  this.update = function(gameData)
+  { 
+    this.showCurrentQuestion(gameData.current_question_id);
+    switch(gameData.state)
+    {
+      case 0: //open
+        // i'm the questionner and it's open so show the questions
+        if(gameData.current_user_id == this.getCurrentUserId())
+        {
+          this.showQuestions();
+        } else {
+          this.showWaitForQuestionMessage();
+        }
+      break;
+      case 1:
+        // i'm the questionner and it's closed so show the wait for answer message
+        if(gameData.current_user_id == getCurrentUserId())
+        {
+          this.showWaitForAnswerMessage();
+        } else {
+          this.showAnswers();
+        }
+      break;
+      case 3:
+        this.showSuccess();
+        this.showNextButton(gameData.current_user_id);
+      break;
+      case 4:
+        this.showFail(game.answer_id);
+        this.showNextButton(gameData.current_user_id);
+      break;
+      default:
+      break;
+    }
+  }
 
-function showAnswers()
-{
-  if(currentWindow == "answers")
-    return;
-  $.get("/questions/"+currentQuestionId+"/answers?game_id="+getGameId(), function (html) {
-    $("#action_space").html(html);
-  });
-  $("#status_space").html("Choose an answer!");
-  currentWindow = "answers";
-}
+  this.getGameId = function()
+  {
+    return this.id;
+  }
 
-function showWaitForQuestionMessage()
-{
-  if(currentWindow == "waitQuestion")
-    return;
-  $("#status_space").html("Wait for a question from the other player...");
-  currentWindow = "waitQuestion";
-  $("#action_space").html("");
-}
+  this.getCurrentUserId = function()
+  {
+    return this.currentUserId;
+  }
 
-function showWaitForAnswerMessage()
-{
-  if(currentWindow == "waitAnswer")
-    return;
-  $("#status_space").html("Ok wait for an answer");
-  currentWindow = "waitAnswer";
-  $("#action_space").html("");
-}
+  this.showNextButton = function(current_user)
+  {
+    if(current_user == this.getCurrentUserId())
+      $("#next_question").show();
+    else
+      $("#next_question").hide();
+  }
 
-function showSuccess()
-{
-  $("#status_space").html("<div class='alert alert-success'>WINNER</div>");
-  $("#action_space").html("");
-}
-function showFail(id)
-{
-  $("#status_space").html("<div class='alert alert-error'>FAIL</div>");
-  //$.get("/questions/"+currentQuestionId+"/answers/"+id+"?game_id="+getGameId(), function(html)
-  //{  
+  this.showCurrentQuestion = function(id)
+  {
+    if(!id)
+      return;
+    if(id != this.currentQuestionId)
+      $.get("/questions/"+id, function(html) {
+        $("#question_space").html(html);
+      });
+    this.currentQuestionId = id;
+  }
+
+  this.showQuestions = function()
+  {
+    $("#next_question").hide();
+    if(this.currentWindow == "questions")
+      return;
+    
+    $.get("/questions?game_id="+this.getGameId(), function (html) {
+      $("#action_space").html(html);
+    });
+    $("#status_space").html("Choose a question!");
+    this.currentWindow = "questions";
+  }
+
+  this.showAnswers = function()
+  {
+    if(this.currentWindow == "answers")
+      return;
+    $.get("/questions/"+this.currentQuestionId+"/answers?game_id="+this.getGameId(), function (html) {
+      $("#action_space").html(html);
+    });
+    $("#status_space").html("Choose an answer!");
+    this.currentWindow = "answers";
+  }
+
+  this.showWaitForQuestionMessage = function()
+  {
+    if(currentWindow == "waitQuestion")
+      return;
+    $("#status_space").html("Wait for a question from the other player...");
+    currentWindow = "waitQuestion";
     $("#action_space").html("");
-  //});
+  }
+
+  this.showWaitForAnswerMessage = function()
+  {
+    if(currentWindow == "waitAnswer")
+      return;
+    $("#status_space").html("Ok wait for an answer");
+    currentWindow = "waitAnswer";
+    $("#action_space").html("");
+  }
+
+  this.showSuccess = function()
+  {
+    $("#status_space").html("<div class='alert alert-success'>WINNER</div>");
+    $("#action_space").html("");
+  }
+  this.showFail = function(id)
+  {
+    $("#status_space").html("<div class='alert alert-error'>FAIL</div>");
+    //$.get("/questions/"+currentQuestionId+"/answers/"+id+"?game_id="+getGameId(), function(html)
+    //{  
+      $("#action_space").html("");
+    //});
+  }
 }
